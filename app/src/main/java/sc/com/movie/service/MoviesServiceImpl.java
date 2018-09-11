314d767e2277e8281b5ec26371de8bfdcbf78fda
GaborMartin <gabormartin94@gmail.com>
2018-09-11 11:30:20 +0200

    Implemented basic MoviesServiceImpl class, getMovies, getMovie methods has been added.

---
+++ app/src/main/java/sc/com/movie/service/MoviesServiceImpl.java
@@ -0,0 +1,34 @@
+package sc.com.movie.service;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import sc.com.movie.model.Movie;
+
+public class MoviesServiceImpl {
+
+    private static final String API_KEY = "43a7ea280d085bd0376e108680615c7f";
+
+    public Call<List<Movie>> getMovies(String apiKey) {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.themoviedb.org/3/")
+                .build();
+
+        MovieService movieService = retrofit.create(MovieService.class);
+
+        return movieService.getMovies(API_KEY);
+    }
+
+    public Call<Movie> getMovie(String apiKey, int movieId) {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.themoviedb.org/3/")
+                .build();
+
+        MovieService movieService = retrofit.create(MovieService.class);
+
+        return movieService.getMovie(API_KEY, movieId);
+    }
+}
